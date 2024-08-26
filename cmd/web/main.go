01b3f9ed4eeb87551e45491c1f3301e5e6dbacc3
Damon <ddiazny919@gmail.com>
2024-08-26 05:44:12 -0400

    isolated routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,46 +1,35 @@
 package main
 
 import (
-  "log/slog"
-  "net/http"
-  "flag"
-  "os"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
 )
 
 // Holds application dependencies, to inject
 type application struct {
-  logger *slog.Logger
+	logger *slog.Logger
 }
 
 func main() {
-  // Config command line flags
-  addy := flag.String("addy", ":4000", "HTTP network address")
-  flag.Parse()
+	// Config command line flags
+	addy := flag.String("addy", ":4000", "HTTP network address")
+	flag.Parse()
 
-  // Structured Logging
-  logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-    AddSource: true,
-  }))
-  
-  // initialization of app
-  app := &application{
-    logger: logger,
-  }
+	// Structured Logging
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
 
-  mux := http.NewServeMux()
+	// initialization of app
+	app := &application{
+		logger: logger,
+	}
 
-  fileServer := http.FileServer(http.Dir("./ui/static/"))
-  mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	logger.Info("starting server", slog.Any("addy", *addy))
 
-  mux.HandleFunc("GET /{$}", app.home) // restrict route to exact match
-  mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-  mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-  mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
-  logger.Info("starting server", slog.Any("addy", *addy))
-
-  err := http.ListenAndServe(*addy, mux)
-  logger.Error(err.Error())
-  os.Exit(1)
+	err := http.ListenAndServe(*addy, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
 }
-
